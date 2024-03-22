e0478bc6bfe2371e80fa957c0e49e034a22f2747
Beefymuffins <98682659+Beefymuffins@users.noreply.github.com>
2024-03-21 22:04:49 -0400

    Added -a flag

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module beef/sun
 
-go 1.18
+go 1.19
 
 require (
 	github.com/fatih/color v1.16.0 // indirect
--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/fatih/color"
@@ -46,18 +48,36 @@ func main() {
 		log.Fatal(".env file couldn't be loaded")
 	}
 
+	// Set Vars
 	api_key := os.Getenv("API_KEY")
 	base_url := "http://api.weatherapi.com/v1/forecast.json"
+
+	// Parse command line flags
+	displayAll := flag.Bool("a", false, "Display all hours")
+	flag.Parse()
+
+	// Set default query param (no city supplied as an argument)
 	q := os.Getenv("DEFAULT_CITY") // Default value
 
-	// If user passes in an city as an argument set it to q
+	// If user passes in a city as an argument set it to q (Also checks for flags, -a)
 	// Example: go run main.go London
-	if len(os.Args) >= 2 {
+	if len(os.Args) >= 2 && !strings.HasPrefix(os.Args[1], "-") {
 		q = os.Args[1]
 	}
 
+	// Build the URL
+	var url string
+	if q != "" {
+		url = base_url + "?key=" + api_key + "&q=" + q + "&days=1&aqi=no&alerts=no"
+		if *displayAll {
+			url += "&a=1"
+		}
+	} else {
+		url = base_url + "?key=" + api_key + "&days=1&aqi=no&alerts=no"
+	}
+
 	// Get response
-	res, err := http.Get("" + base_url + "?key=" + api_key + "&q=" + q + "&days=1&aqi=no&alerts=no")
+	res, err := http.Get(url)
 
 	// Check for Error
 	if err != nil {
@@ -114,39 +134,36 @@ func main() {
 	for _, hour := range hours {
 		date := time.Unix(hour.TimeEpoch, 0)
 
-		// Display only future hours
-		if date.Before(time.Now()) {
-			continue
+		// Display all hours if -a flag is provided
+		if *displayAll || date.After(time.Now()) {
+			// Change hour.TempF text color based on temp
+			var tempColorFunc func(format string, a ...interface{}) string
+			if hour.TempF > 80 {
+				tempColorFunc = color.RedString
+			} else if hour.TempF < 32 {
+				tempColorFunc = color.BlueString
+			} else {
+				tempColorFunc = fmt.Sprintf
+			}
+
+			// Change hour.ChanceOfRain text color based on ChanceOfRain %
+			var rainColor string
+			if hour.ChanceOfRain > 40 {
+				rainColor = color.RedString("%.0f%%", hour.ChanceOfRain)
+			} else {
+				rainColor = fmt.Sprintf("%.0f%%", hour.ChanceOfRain)
+			}
+
+			// Assign the values to message
+			message := fmt.Sprintf(
+				"%s - %s, %s, %s\n",
+				date.Format("15:04"),
+				tempColorFunc("%.0fF", hour.TempF),
+				rainColor,
+				hour.Condition.Text,
+			)
+
+			fmt.Print(message)
 		}
-
-		// Change hour.TempF text color based on temp
-		var tempColorFunc func(format string, a ...interface{}) string
-		if hour.TempF > 80 {
-			tempColorFunc = color.RedString
-		} else if hour.TempF < 32 {
-			tempColorFunc = color.BlueString
-		} else {
-			tempColorFunc = fmt.Sprintf
-		}
-
-		// Change hour.ChanceOfRain text color based on ChanceOfRain %
-		var rainColor string
-		if hour.ChanceOfRain > 40 {
-			rainColor = color.RedString("%.0f%%", hour.ChanceOfRain)
-		} else {
-			rainColor = fmt.Sprintf("%.0f%%", hour.ChanceOfRain)
-		}
-
-		// Assign the values to message
-		message := fmt.Sprintf(
-			"%s - %s, %s, %s\n",
-			date.Format("15:04"),
-			tempColorFunc("%.0fF", hour.TempF),
-			rainColor,
-			hour.Condition.Text,
-		)
-
-		// Print the message
-		fmt.Print(message)
 	}
 }
